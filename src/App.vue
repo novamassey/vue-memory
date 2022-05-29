f4b2b6572807798c72fbee2990f5b4daabc39ee5
Lauren N Massey <laurennmassey@Laurens-MacBook-Pro.local>
2022-05-29 11:51:05 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,101 @@
+<template>
+  <h1>Memory Game</h1>
+
+  <section class="game-board">
+    <CardComp
+      v-for="(card, index) in cardList"
+      :value="card.value"
+      :key="index"
+      :matched="card.matched"
+      :visible="card.visible"
+      :position="card.position"
+      @select-card="flipCard"
+    />
+  </section>
+  <h2>{{ status }}</h2>
+</template>
+
+<script>
+import { ref, watch } from "vue";
+import CardComp from "./components/CardComp";
+
+export default {
+  name: "App",
+  components: { CardComp },
+  //setup is vanilla JS so you must return
+  setup() {
+    const cardList = ref([]);
+    const userSelection = ref([]);
+    let status = ref("");
+    for (let i = 0; i < 16; i++) {
+      cardList.value.push({
+        value: i,
+        visible: false,
+        position: i,
+        matched: false,
+      });
+    }
+    const flipCard = (payload) => {
+      cardList.value[payload.position].visible = true;
+      if (userSelection.value[0]) {
+        userSelection.value[1] = payload;
+      } else {
+        userSelection.value[0] = payload;
+      }
+    };
+    watch(
+      userSelection,
+      (currentValue) => {
+        if (currentValue.length === 2) {
+          // console.log(currentValue.value, currentValue[0])
+          const cardOne = currentValue[0];
+          const cardTwo = currentValue[1];
+
+          if (cardOne.faceValue === cardTwo.faceValue) {
+            status.value = "matched!";
+            cardList.value[cardOne.position].matched = true;
+            cardList.value[cardTwo.position].matched = true;
+          } else {
+            status.value = "mismatch!";
+            cardList.value[cardOne.position].visible = false;
+            cardList.value[cardTwo.position].visible = false;
+          }
+
+          userSelection.value.length = 0;
+        }
+      },
+      { deep: true }
+    );
+    return {
+      cardList,
+      flipCard,
+      userSelection,
+      status,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.game-board {
+  display: grid;
+  grid-template-columns: 100px 100px 100px 100px;
+  grid-template-rows: 100px 100px 100px 100px;
+  column-gap: 30px;
+  row-gap: 30px;
+  justify-content: center;
+}
+.card {
+  border: 5px solid gray;
+  height: 100px;
+  width: 100px;
+}
+</style>
---
+++ src/components/CardComp.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+  props: {
+    position: {
+      type: Number,
+      required: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+    visible: { type: Boolean, default: false },
+    matched: { type: Boolean, default: false },
+  },
+  setup(props, context) {
+    const selectCard = () => {
+      context.emit("select-card", {
+        position: props.position,
+        faceValue: props.value,
+      });
+    };
+    return {
+      selectCard,
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="card" @click="selectCard">
+    <div class="card-face is-front" v-if="visible">
+      {{ value }}-{{ matched }}
+    </div>
+    <div class="card-face is-back" v-else>Back</div>
+  </div>
+</template>
+
+<style>
+.card {
+  border: 5px solid gray;
+  position: relative;
+}
+.card-face {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+}
+.card-face.is-front {
+  background-color: red;
+  color: white;
+}
+.card-face.is-back {
+  background-color: blue;
+  color: white;
+}
+</style>
